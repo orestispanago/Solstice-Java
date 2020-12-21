ce36c8aed751010bd6214f421409125db8cd8b68
orestispanago <orestispanago@gmail.com>
2020-12-21 15:19:26 +0200

    Can set PATHs for solstice from SolsticeService

--- src/main/java/services/SolsticeService.java
+++ src/main/java/services/SolsticeService.java
@@ -1,14 +1,26 @@
 package services;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class SolsticeService {
+
+    public static void setPaths(ProcessBuilder pb) {
+        Map<String, String> env = pb.environment();
+        String PATH = System.getenv("PATH");
+        PATH += ":/home/orestis/Programs/Solstice-0.9.0-GNU-Linux64/bin";
+        env.put("PATH", PATH);
+        env.put("LD_LIBRARY_PATH", "/home/orestis/Programs/Solstice-0.9.0-GNU-Linux64/lib");
+        env.put("MANPATH", "/home/orestis/Programs/Solstice-0.9.0-GNU-Linux64/share/man");
+        System.out.println(env);
+    }
 
     public static void run(String cmd) throws IOException {
         String[] cmdArr = cmd.split(" ");
         ProcessBuilder pb = new ProcessBuilder().inheritIO().command(cmdArr);
         pb.redirectErrorStream(true);
         pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+//        setPaths(pb);
 //        File logFile = new File("out.txt");
 //        pb.redirectOutput(logFile);
         pb.start();
